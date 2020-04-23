7c15e92af7ef7237e38bb52297831ed3c729ff57
Uniyal, Utkarsh <Utkarsh.Uniyal@gmail.com>
2020-04-23 21:59:48 +0530

    data fetch link changed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import Counter from './components/Counter'
 import './App.css';
 import './styles/landing.css'
-import News from './components/News'
 
 function App() {
   return (
     <div className="App">
       <h1>COVID-19 TRACKER</h1>
       <Counter/>
-      <News/>
     </div>
   );
 }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,70 +1,51 @@
 import React from 'react'
-import TotalInfected from './TotalInfected'
-import TotalDeaths from './TotalDeaths'
-import TotalRecovered from './TotalRecovered'
 import '../styles/landing.css'
-const csv=require('csvtojson')
+import CountUp from 'react-countup'
 
 
 
 class Counter extends React.Component{
 
     state={
-        infected:[{count:0}],
-        deaths:[{count:0}],
-        recovered:[{count:0}]
+        infected:0,
+        deaths:0,
+        recovered:0
     }
 
     componentDidMount(){
-        Promise.all([
-        fetch("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv",{
-            mode:'cors',
-            headers:{
-                "Content-type": "text/plain; charset=utf-8"
-            }
-        })
-        .then(res=>res.text())
-        .then(res=>{
-            csv().fromString(res)
-            .then(obj=>{
-                this.setState({infected:obj})
-            })
-        }),
-        fetch("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/archived_data/archived_time_series/time_series_2019-ncov-Deaths.csv",{
-            mode:'cors',
-            headers:{
-                "Content-type": "text/plain; charset=utf-8"
-            }
-        })
-        .then(res=>res.text())
-        .then(res=>{
-            csv().fromString(res)
-            .then(obj=>{
-                this.setState({deaths:obj})
-            })
-        }),
-        fetch("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/archived_data/archived_time_series/time_series_2019-ncov-Recovered.csv",{
-            mode:'cors',
-            headers:{
-                "Content-type": "text/plain; charset=utf-8"
-            }
-        })
-        .then(res=>res.text())
-        .then(res=>{
-            csv().fromString(res)
-            .then(obj=>{
-                this.setState({recovered:obj})
-            })
-        })
-        ])
+        fetch("https://covid19.mathdro.id/api")
+        .then(res=>res.json())
+        .then(res=>
+            this.setState({infected:res["confirmed"]["value"],
+            deaths:res["deaths"]["value"],
+            recovered:res["recovered"]["value"]
+        }))
     }
 
     render(){
         return(
         <div className="all-counts">
-            <TotalInfected value={this.state.infected}/>
-            <TotalDeaths value={this.state.deaths}/>
-            <TotalRecovered value={this.state.recovered}/>
+            <div className="counter">
+                <p>Total Infected</p>
+            <CountUp
+                end={this.state.infected}
+                duration={5}    
+            />
+            </div>
+            <div className="counter">
+                <p>Total Recovered</p>
+            <CountUp
+                end={this.state.recovered}
+                duration={5}
+            />
+            </div>
+            <div className="counter">
+                <p>Total Deaths</p>
+            <CountUp
+                end={this.state.deaths}
+                duration={5}
+            />
+            </div>
         </div>
         )
     }
